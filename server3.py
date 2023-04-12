d1f3f8df67f20f9f581db9a2ab01295e3539f473
Henry Oyedeji <henryoyed@gmail.com>
2023-04-12 15:19:29 +0100

    Fixed errors in Datapoint and Ratio errors

--- server3.py
+++ server3.py
@@ -216,7 +216,7 @@ def get(req_handler, routes):
                 return
 
 
-def run(routes, host='0.0.0.0', port=8080):
+def run(routes, host='0.0.0.0', port=8000):
     """ Runs a class as a server whose methods have been decorated with
         @route.
     """
@@ -337,4 +337,5 @@ if __name__ == '__main__':
     if not os.path.isfile('test.csv'):
         print("No data found, generating...")
         generate_csv()
+
     run(App())
